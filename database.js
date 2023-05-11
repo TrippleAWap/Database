baabb394a50a0b411de8841043872e9f92220c12
TrippleAWap <90356816+TrippleAWap@users.noreply.github.com>
2023-05-11 17:10:29 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,63 +1,55 @@
-import {world, system} from "@minecraft/server";
+import { world, system } from "@minecraft/server";
 
 const overworld = world.getDimension("overworld");
+
 export class Database {
     /**
      * @param {string} databaseName - The name of the database
      */
     constructor(databaseName) {
-        try {
-            this.databaseName = databaseName;
-            /**@private */
-            this.objective = world.scoreboard.getObjective(databaseName) ?? world.scoreboard.addObjective(databaseName, databaseName);
-            this.modified = false;
-            /**@private */
-            this.data = this.objective.getParticipants().length > 0 ? Object.fromEntries(this.objective.getParticipants().map(x => {
-                const [key, value] = x.displayName.split(":");
-                return [key, JSON.parse(value)];
-                })) : {};
-            /**@private */
-            this.createProxy = (target) => {
-                return new Proxy(target, {
-                    get: (target, key) => {
-                        if (Array.isArray(target[key])) {
-                            // If the property is an array, wrap it with a Proxy
-                            return this.createProxy(target[key]);
-                        } else {
-                            return target[key];
-                        }
-                    },
-                    set: (target, key, value) => {
-                        target[key] = value;
-                        if (!this.modified)
-                            (this.modified = true) &&
-                            system.run(() => {
-                                this.save();
-                                this.modified = false;
-                            });
-                        return true;
-                    },
-                    deleteProperty: (target, key) => {
-                        delete target[key];
-                        if (!this.modified)
-                            (this.modified = true) &&
-                            system.run(() => {
-                                this.save();
-                                this.modified = false;
-                            });
-                        return true;
-                    },
-                    has: (target, key) => {
-                        return key in target;
-                    },
-                    ownKeys: (target) => {
-                        return Reflect.ownKeys(target);
-                    },
-                });
-            }
-            /**@private */
-            this.proxy = this.createProxy(this.data);
-        } catch {}
+        this.databaseName = databaseName;
+        /**@private */
+        this.objective = world.scoreboard.getObjective(databaseName) ?? world.scoreboard.addObjective(databaseName, "{}");
+        /**@private */
+        this.data = this.objective.getParticipants().length? JSON.parse(`{${this.objective.getParticipants().map((e) => e.displayName.replace(/\\"/g, '"')).join(",")}}`) : {};
+        /**@private */
+        this.modified = false;
+        /**@private */
+        this.createProxy = (target) => {
+            return new Proxy(target, {
+                get: (target, key) => {
+                    if (Array.isArray(target[key])) {
+                        // If the property is an array, wrap it with a Proxy
+                        return this.createProxy(target[key]);
+                    } else {
+                        return target[key];
+                    }},
+                set: (target, key, value) => {
+                    target[key] = value;
+                    if (!this.modified)
+                        (this.modified = true) &&
+                        system.run(() => {
+                            this.save();
+                            this.modified = false;
+                        });
+                    return true;},
+                deleteProperty: (target, key) => {
+                    delete target[key];
+                    if (!this.modified)
+                        (this.modified = true) &&
+                        system.run(() => {
+                            this.save();
+                            this.modified = false;
+                        });
+                    return true;},
+                has: (target, key) => {
+                    return key in target;},
+                ownKeys: (target) => {
+                    return Reflect.ownKeys(target);},
+            });
+        }
+        /**@private */
+        this.proxy = this.createProxy(this.data);
     }
 
     /**
@@ -74,11 +66,10 @@ export class Database {
      * @summary Use of this method is not recommended as it may not correctly save the database data.
      */
     save() {
-
-        try { world.scoreboard.removeObjective(this.databaseName) } catch (e) { console.warn(e) }
+        try { world.scoreboard.removeObjective(this.databaseName); } catch { };
         world.scoreboard.addObjective(this.databaseName, this.databaseName);
-        for (const [key, value] of Object.entries(this.proxy)) {
-            try { overworld.runCommandAsync(`scoreboard players set "${key}:${JSON.stringify(value).replace(/"/g, '\\"')}" ${this.databaseName} 0`) } catch (e) { console.warn(e) }
+        for (const key in this.data) {
+            overworld.runCommandAsync(`scoreboard players set "\\"${key}\\":${JSON.stringify(this.data[key]).replace(/"/g, '\\"')}" ${this.databaseName} 0`);
         }
     }
 }
